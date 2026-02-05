45875d4aaf32a561c822ae5c0def575aacbd9fea
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-02-06 00:00:16 +0700

    fix: apply all four sorting algorithm

--- logic_nolep/ln-sortingAlgo.js
+++ logic_nolep/ln-sortingAlgo.js
@@ -85,16 +85,39 @@ const sortAlgo = {
 };
 
 const groupAnagrams = function (strs) {
-  let arr = strs.map((str) => {
-    return sortAlgo.selectionSort(str.split(""));
-  });
-
-  let obj = {};
-  for (let i = 0; i < strs.length; i++) {
-    if (!obj[arr[i]]) obj[arr[i]] = [];
-    obj[arr[i]].push(strs[i]);
+  // early exit
+  if (strs.length <= 1) {
+    return {
+      bubble: [strs],
+      selection: [strs],
+      insertion: [strs],
+      merge: [strs],
+    };
   }
-  return Object.values(obj);
+
+  const len = strs.length;
+  const split = strs.map((str) => [...str]);
+
+  const algorithm = {
+    bubble: sortAlgo.bubbleSort,
+    selection: sortAlgo.selectionSort,
+    insertion: sortAlgo.insertionSort,
+    merge: sortAlgo.mergeSort,
+  };
+
+  let groupedByAlgorithm = {};
+  for (const [name, algo] of Object.entries(algorithm)) {
+    const sortedArr = split.map((str) => algo([...str]));
+    let obj = {};
+    for (let i = 0; i < len; i++) {
+      const key = sortedArr[i].join("");
+      if (!obj[key]) obj[key] = [];
+      obj[key].push(strs[i]);
+    }
+    groupedByAlgorithm[name] = Object.values(obj);
+  }
+
+  return groupedByAlgorithm;
 };
 
 // Test Case 1
@@ -105,22 +128,22 @@ console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
 console.log(groupAnagrams([""]));
 // Output: [[""]]
 
-// // Test Case 3
+// Test Case 3
 console.log(groupAnagrams(["a"]));
 // Output: [["a"]]
 
-// // Test Case 4
+// Test Case 4
 console.log(groupAnagrams(["listen", "silent", "hello", "world"]));
-// // Output: [["listen","silent"],["hello"],["world"]]
+// Output: [["listen","silent"],["hello"],["world"]]
 
-// // Test Case 5
+// Test Case 5
 console.log(groupAnagrams(["rat", "tar", "art", "car"]));
 // Output: [["rat","tar","art"],["car"]]
 
-// // Test Case 6
+// Test Case 6
 console.log(groupAnagrams(["apple", "banana", "leapp", "grape", "orange"]));
 // Output: [["apple","leapp"],["banana"],["grape"],["orange"]]
 
-// // Test Case 7
+// Test Case 7
 console.log(groupAnagrams(["abcd", "dcba", "xyz", "zyx", "wxyz"]));
 // Output: [["abcd","dcba"],["xyz","zyx"],["wxyz"]]
